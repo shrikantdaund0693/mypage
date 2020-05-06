d176579e9d97a6be4032b11af8c81ea1cdaee5b0
Shrikant Daund <shrikantdaund0693@gmail.com>
2020-05-06 14:50:51 +0530

    mywebtemplate

---
+++ Shrikant/script.js
@@ -0,0 +1,6 @@
+function funct(){
+    alert("You are at Homepage.........");
+    confirm("You want to continue........");
+    var name=prompt("Enter Your name please.....");
+    confirm("Your name entered is"+ " "+name);
+};
